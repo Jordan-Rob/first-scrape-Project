66c664ac0098db36f3c73cede642d6e9c326bd29
jordanrob709 <jordanrob709@gmail.com>
2019-07-05 17:21:44 +0300

    initial commit

---
+++ spiders/jum_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class GoodReadsSpider(scrapy.Spider):
+    #identity
+    name = 'jum'
+    #requests
+    def start_requests(self):
+        url = 'https://www.jumia.ug/laptops?page=1'
+       
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    #response
+    def parse(self, response):
+        for product in response.selector.xpath("//a[@class='link']"):
+            price = product.xpath(".//span[@dir='ltr']/@data-price").extract_first()
+            new_price = int(price)
+
+            if (new_price < 1500000):
+                yield {
+                    'name':product.xpath(".//span[@class='name']/text()").extract_first(),
+                    'price':product.xpath(".//span[@dir='ltr']/@data-price").extract_first()
+                }
+            
+
+        next_page = response.selector.xpath("//a[@title='Next']/@href").extract_first()
+
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
+        
